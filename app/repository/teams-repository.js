6290d287cd25ad0c9ad3c0da4f7416d9cb21aa3a
Sean Sica <23294618+seansica@users.noreply.github.com>
2023-09-07 11:45:42 -0400

    Start expanding the new Teams repository DAO

--- app/repository/teams-repository.js
+++ app/repository/teams-repository.js
@@ -1,6 +1,8 @@
+const AbstractRepository = require('../repository/repository.abstract');
 const Team = require('../models/team-model'); // Import the Team model appropriately
+const { DatabaseError } = require('../exceptions');
 
-exports.findTeamsByUserId = function(userAccountId, options) {
+exports.findTeamsByUserId = async function (userAccountId, options) {
     const aggregation = [
         { $sort: { 'name': 1 } },
         { $match: { userIDs: { $in: [userAccountId] } } },
@@ -14,6 +16,18 @@ exports.findTeamsByUserId = function(userAccountId, options) {
             }
         }
     ];
+    try {
+        return await Team.aggregate(aggregation).exec();
+    } catch (err) {
+        throw new DatabaseError(err);
+    }
 
-    return Team.aggregate(aggregation).exec();
 };
+
+/**
+ * If a function is not directly found on the exports object in repository.abstract.js, 
+ * JavaScript will look up its prototype chain to find it. If it exists on the abstractRepo, 
+ * it will use that. This effectively allows the concrete repository to "inherit" methods 
+ * from the abstract repository.
+ */
+Object.setPrototypeOf(exports, AbstractRepository);
